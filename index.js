25404d728194c18e6ea117adfbce6d42532b158c
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2020-05-04 16:14:27 +0200

    Added update order functionality

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql')
+const bodyParser = require('body-parser');
+const _ = require('underscore');
 
 const SELECT_PRODUCTS = "SELECT * FROM product";
 const SELECT_SECTIONS = "SELECT * FROM lidl_section";
-const SELECT_CART = "SELECT cart.uid, cart.unit, cart.quantity, cart.productId, lidl_section.id AS sectionId, lidl_section.name AS sectionName, product.name AS productName FROM cart LEFT JOIN (lidl_section, product) ON cart.productId = product.id AND product.section = lidl_section.id;";
+const SELECT_CART = "SELECT cart.uid, cart.unit, cart.quantity, cart.productId, lidl_section.id AS sectionId, " +
+    "lidl_section.name AS sectionName, product.name AS productName FROM cart LEFT JOIN (lidl_section, product) " +
+    "ON cart.productId = product.id AND product.section = lidl_section.id;";
 
 const app = express()
 const connection = mysql.createConnection({
@@ -20,6 +24,9 @@ connection.connect(err => {
 });
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 app.get('/', (req, res) => {
     res.send('Hello')
@@ -78,10 +85,48 @@ app.get('/supported-stores', (req, res) => {
     })
 })
 
+app.all('/supported-stores/updateOrder', (req,res) => {
+    const {store} = (req.query);
+    const orderJson = req.body;
+    let baseString = `INSERT INTO ${store}(id, sectionId, sectionOrder) VALUES `
+    let endString = ` ON DUPLICATE KEY UPDATE sectionOrder=VALUES(sectionOrder);`
+    let first = true
+    for(let entry of orderJson){
+        if(first){
+            first = !first
+        }else{
+            baseString += ','
+        }
+        baseString += `(${entry.id}, ${entry.sectionId}, ${entry.sectionOrder})`
+    }
+    connection.query(baseString + endString, (err, results) => {
+        if (err){
+            console.log(err)
+            return res.send(err)
+        }else{
+            // connection.query(`SELECT * FROM ${store};`, (err, results) => {
+            //     if (err){
+            //         console.log(err)
+            //         return res.send(err)
+            //     }else{
+            //         console.log(results)
+            //         return res.json(req.query);
+            //     }
+            // })
+            console.log(results)
+            return res.json(req.query);
+        }
+    })
+    connection.commit()
+})
+
 app.get('/products/add', (req,res) => {
     const {name, default_unit, section} = req.query;
     const INSERT_PRODUCT = `INSERT INTO product(name, default_unit, section) 
-    VALUES ('${name.replace('_', ' ')}', '${default_unit}', ${section});`
+    VALUES ('${name.replace('_', ' ')}', '${default_unit}', ${section});
+    SELECT lidlSredzka.id, lidlSredzka.sectionId,lidl_section.name AS sectionName, 
+                lidlSredzka.sectionOrder FROM ${result.tableReference} LEFT JOIN lidl_section ON 
+                lidlSredzka.sectionId = lidl_section.id;`
     connection.query(INSERT_PRODUCT, (err, results) => {
         if (err){
             return res.send(err)
@@ -94,23 +139,67 @@ app.get('/products/add', (req,res) => {
 
 app.get('/cart/add', (req,res) => {
     const {productId, unit, quantity} = req.query;
-    const INSERT_PRODUCT = `INSERT INTO cart(productId, unit, quantity) 
-    VALUES (${productId}, '${unit}', ${quantity});`
+    const INSERT_PRODUCT = `INSERT INTO cart(productId, unit, quantity) VALUES (${productId}, '${unit}', ${quantity});`
     connection.query(INSERT_PRODUCT, (err, results) => {
+        if (err){
+            console.log(err)
+            return res.send(err)
+        }else{
+            const SELECT_PRODUCT_AS_CART_PRODUCT = `SELECT cart.uid, cart.unit, cart.quantity, cart.productId, lidl_section.id AS sectionId, lidl_section.name AS sectionName, product.name AS productName FROM cart, lidl_section, product WHERE cart.productId = product.id AND product.section = lidl_section.id AND cart.productId = ${productId};`
+            return connection.query(SELECT_PRODUCT_AS_CART_PRODUCT, (err, results) => {
+                if (err){
+                    console.log(err)
+                    return res.send(err)
+                }else{
+                    console.log(results)
+                    return res.json(results)
+                }});
+        }
+    })
+    connection.commit();
+})
+
+app.get('/cart/updateProduct', (req,res) => {
+    const {uid, productId, unit, quantity} = req.query;
+    const UPDATE_PRODUCT = `UPDATE cart SET quantity=${quantity}, unit='${unit}'
+    WHERE uid=${uid};`
+    connection.query(UPDATE_PRODUCT, (err, results) => {
         if (err){
             return res.send(err)
         }else{
             return res.json(req.query);
         }
     })
-    connection.commit();
+    connection.commit()
+})
+
+app.get('/cart/deleteProduct', (req,res) => {
+    const {uid} = req.query;
+    connection.query(`DELETE FROM cart WHERE uid=${uid}`, (err, results) => {
+        if (err){
+            return res.send(err)
+        }else{
+            return res.json(req.query);
+        }
+    })
+    connection.commit()
 })
 
 app.get('/cart/update', (req,res) => {
-    const {uid, productId, unit, quantity} = req.query;
-    const UPDATE_PRODUCT = `UPDATE cart SET quantity=${quantity}, unit='${unit}', productId=${productId}
-    WHERE uid=${uid};`
-    connection.query(UPDATE_PRODUCT, (err, results) => {
+    const {cart} = (req.query);
+    const cartJson = JSON.parse(cart);
+    let baseString = "INSERT INTO cart(uid, productId, unit, quantity) VALUES "
+    let endString = " ON DUPLICATE KEY UPDATE unit=VALUES(unit), quantity=VALUES(quantity)"
+    let first = true
+    for(let entry of cartJson){
+        if(first){
+            first = !first
+        }else{
+            baseString += ','
+        }
+        baseString += `(${entry.uid}, ${entry.productId}, '${entry.unit}', ${entry.quantity})`
+    }
+    connection.query(baseString + endString, (err, results) => {
         if (err){
             return res.send(err)
         }else{
